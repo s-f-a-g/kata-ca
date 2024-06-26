96ece6982b540b8ea517179f3a9045841fe3f17a
Sebastien Faggin <sebastien.faggin@eu.corp.airliquide.com>
2024-06-26 17:03:22 +0200

    KATA_CURRENCY_CONVERTER_0003 polling init ok

--- src/app/components/currency-converter/currency-converter.component.ts
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, inject, model, ModelSignal, signal} from '@angular/core';
+import {Component, computed, inject, model, ModelSignal, OnInit} from '@angular/core';
 import {CurrencyConverterService} from "./currency-converter.service";
 import {FormsModule} from "@angular/forms";
 import {DecimalPipe} from "@angular/common";
@@ -19,7 +19,6 @@ export class CurrencyConverterComponent {
   originalValue: ModelSignal<number> = model(100);
   variableRate: ModelSignal<number> = this.currencyConverterService.variableRate;
   fixRate: ModelSignal<number> = model(1.5);
-
   applicableRate = computed(() => {
     if (this.variation() > 2) {
       return this.variableRate();
@@ -27,6 +26,7 @@ export class CurrencyConverterComponent {
       return this.fixRate()
     }
   });
+
   variation = computed(() => {
     const fromFixRateValue = this.originalValue() * this.fixRate();
     const fromVariableRateValue = this.originalValue() * this.variableRate();
--- src/app/components/currency-converter/currency-converter.service.ts
+++ src/app/components/currency-converter/currency-converter.service.ts
@@ -1,4 +1,5 @@
-import {computed, Injectable, model, ModelSignal} from '@angular/core';
+import {computed, effect, Injectable, model, ModelSignal} from '@angular/core';
+import {interval} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,21 @@ import {computed, Injectable, model, ModelSignal} from '@angular/core';
 export class CurrencyConverterService {
 
   variableRate: ModelSignal<number> = model(1.1)
-  constructor() { }
+
+  constructor() {
+    effect(() => interval(3000)
+      .pipe()
+      .subscribe(_ => {
+        this.updateVariableRate();
+      })
+    )
+  }
+
+  updateVariableRate(): void {
+    const min = -0.05;
+    const max = +0.05;
+    const newRate = ((Math.random() * max) + (Math.random() * min)) + 1;
+    console.log('newRate', newRate);
+    this.variableRate.set(newRate);
+  }
 }
