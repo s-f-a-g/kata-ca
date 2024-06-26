d7a4a99a2405f023cac65e781e460af3ce67f479
Sebastien Faggin <sebastien.faggin@eu.corp.airliquide.com>
2024-06-26 18:09:11 +0200

    KATA_CURRENCY_CONVERTER_0010 WIP historization reached the 2 hours before finishing historization

--- src/app/components/currency-converter/currency-converter.service.ts
+++ src/app/components/currency-converter/currency-converter.service.ts
@@ -1,7 +1,7 @@
 import {effect, Injectable, model, ModelSignal} from '@angular/core';
 import {interval} from "rxjs";
 
-export type History = { realRate: number, fixedRate: number, initialValue: number, convertedValue: number };
+export type History = { realRate: number, fixedRate: number, initialValue: number, convertedValue: string };
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export type History = { realRate: number, fixedRate: number, initialValue: numbe
 export class CurrencyConverterService {
 
   variableRate: ModelSignal<number> = model(1.1)
-  history: ModelSignal<History[] |any> = model([]);
+  // history: ModelSignal<History[]> = model([]); // Add any to compile but in real life I would reject merge request with that. Reached the 2 hours
 
   constructor() {
     effect(() => interval(3000)
@@ -30,8 +30,8 @@ export class CurrencyConverterService {
   }
 
   updateHistory(): void {
-    const newHistory: History[] = [...this.history(), ({ realRate: 1, fixedRate: 1.02, initialValue: 100, convertedValue: 122 })];
-    this.history.set(newHistory)
+   /* const newHistory: History[] = [...this.history(), ({ realRate: 1, fixedRate: 1.02, initialValue: 100, convertedValue: 122 })];
+    this.history.set(newHistory)*/
   }
 
 }
